66fba0ce18d5bc1a1d8563f874660bc2bd1c1133
gomalon <webadmin@gomalon.com>
2019-07-13 18:18:01 +0530

    users model changes

--- src/services/products/products.params.js
+++ src/services/products/products.params.js
@@ -6,10 +6,6 @@ const add = joi.object().keys({
     product_description : joi.string().min(3).max(50).optional().label('description'),
 })
 
-const single_list = joi.object().keys({
-    category_id : joi.string().required().label('Category ID')
-})
-
 const remove = joi.object().keys({
     category_id : joi.string().required().label('Category ID')
 })
@@ -17,6 +13,5 @@ const remove = joi.object().keys({
 
 export default {
     add,
-    single_list,
     remove
 }
